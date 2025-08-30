ac756addf878fdc3c0747c9cc95f807c0b268b50
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-08-30 07:49:53 +0600

    create file  and solve problem in C++

---
+++ area OfCircle.cpp
@@ -0,0 +1,41 @@
+// ================================================
+//               Language: C++ / Cpp
+//               Topic: Solving Problems
+// ================================================
+
+// ===== 👉👉  🔹🔹 Question 1️⃣ : Calculate the area of a Circle (Unprofessional style example) =====
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float r = 5;
+    float a = r * r;
+    cout << "Area is: " << a * 3.1416;
+    cout << endl;
+    return 0;
+}
+
+// ===== 👉👉  🔹🔹 Question 1️⃣ : Calculate the area of a Circle (Professional style example 1) =====
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float radius = 4.3;
+    cout << "Area is: " << radius * radius * 3.1416;
+    cout << endl;
+    return 0;
+}
+
+// ===== 👉👉 🔹🔹 Question 1️⃣ : Calculate the area of a Circle (Professional style example 2) =====
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float r = 4.3;
+    cout << "Area is: " << r * r * 3.1416;
+    cout << endl;
+    return 0;
+}
